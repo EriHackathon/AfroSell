c19ec24babbf54dd68c8ee9fb8d727a107ec8bea
davefabb14 <dvwish9214@gmail.com>
2019-02-14 22:56:40 -0800

    comments and exceptions are added

--- product/src/main/java/com/erihackton/shopping/pro/ProductController.java
+++ product/src/main/java/com/erihackton/shopping/pro/ProductController.java
@@ -1,34 +1,75 @@
-package com.erihackton.shopping.pro;
-
+ppackage com.erihackton.shopping.pro;
+/*
+ * This class is about product controller class 
+ * developed by Dawit Tekle
+ */
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erihackton.shopping.exceptions.ProductNotFoundException;
+
 @RestController
 public class ProductController {
 	@Autowired
 	private ProductServices productService;
+	/*
+	 * this methods  retrieves  all the product
+	 * 
+	 */
 	@RequestMapping("/products")
-    public List<Product> getAllProducts() {
-	   return productService.getAllProducts();
+    public ResponseEntity<Object> getAllProducts() {
+		List<Product> listp;
+		
+		listp=productService.getAllProducts();
+	    if (listp.size()==0) throw new ProductNotFoundException();
+	      return new ResponseEntity<>(listp,HttpStatus.OK);
+	   
      }
-	@RequestMapping("/products/{id}")
-    public Product getProduct(@PathVariable int id) {
-		return productService.getProduct(id);
+	/*
+	 * retrieves specific product using product Id 
+	 * 	product id as parameter 
+	 */
+	
+	 @RequestMapping("/products/{id}")
+	 
+    public ResponseEntity<Object> getProduct(@PathVariable int id) {
+		Product p= productService.getProduct(id);
+		if(p==null) throw new ProductNotFoundException();
+		 return new ResponseEntity<>(p,HttpStatus.OK);
 	}
+	 /*
+	  * This method adds product to the table 
+	  * product as parameter 
+	  */
+	 
 	@RequestMapping(method=RequestMethod.POST,value="/products")
     public void addProduct(@RequestBody() Product pro) {
+		if(pro!=null) {
 		productService.addProduct(pro);
+		}
 	}
-	@RequestMapping(method=RequestMethod.PUT,value="/products/{id}")
+	/*
+	 * 	This methods update the  product using  Product Id 
+	 * It takes  product Instance and product Id as parameter 
+	 */
+	  @RequestMapping(method=RequestMethod.PUT,value="/products/{id}")
+	 
    public void updateProduct(@RequestBody Product pro,@PathVariable int id) {
 			productService.updateProduct(pro,id);
 		}
+	  /*
+	   * This method deletes  product using  product Id 
+	   * It takes product Id as Parameter
+	   */
 	@RequestMapping(method=RequestMethod.DELETE,value="/products/{id}")
 	public void deleteTopic(@PathVariable int id) {
 			productService.deleteProduct(id);
