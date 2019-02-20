b6f7b330232d83b3527bb5bebb4bb4c3ae42881a
davefabb14 <dvwish9214@gmail.com>
2019-02-19 23:44:39 -0800

    Exceptions are updated

--- product/src/main/java/com/erihackton/shopping/exceptions/ProductExceptionController.java
+++ product/src/main/java/com/erihackton/shopping/exceptions/ProductExceptionController.java
@@ -1,17 +1,23 @@
 package com.erihackton.shopping.exceptions;
 
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ProductExceptionController {
+public class ProductExceptionController extends  ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(value=ProductNotFoundException.class)
 			
-    public ResponseEntity<Object> exception(ProductNotFoundException ex){
-		return new ResponseEntity<>("Product Not FOUND ",HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> exception(Exception ex,WebRequest request){
+		ErrorMessage errorMessage= new ErrorMessage(new Date(),ex.getMessage(),request.getDescription(false));
+		return new ResponseEntity<>(errorMessage,new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
 	
 	}
 
--- product/src/main/java/com/erihackton/shopping/exceptions/ProductNotFoundException.java
+++ product/src/main/java/com/erihackton/shopping/exceptions/ProductNotFoundException.java
@@ -8,5 +8,8 @@ public class ProductNotFoundException extends RuntimeException{
 
 	
 	private static final long serialVersionUID = 1L;
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
 
 }
