ad8be7772b1f78043e36e6fd393445ebcab12392
davefabb14 <dvwish9214@gmail.com>
2019-02-25 14:21:49 +0300

    updated Controller

--- product/src/main/java/com/erihackton/shopping/pro/ProductController.java
+++ product/src/main/java/com/erihackton/shopping/pro/ProductController.java
@@ -1,7 +1,7 @@
 ppackage com.erihackton.shopping.pro;
 /*
  * This class is about product controller class 
- * developed by Dawit Tekle 
+ * developed by Dawit Tekle
  */
 import java.util.List;
 
@@ -21,6 +21,8 @@ import com.erihackton.shopping.exceptions.ProductNotFoundException;
 public class ProductController {
 	@Autowired
 	private ProductServices productService;
+	@CrossOrigin(origins = "*")
+
 	/*
 	 * this methods  retrieves  all the product
 	 * 
@@ -30,10 +32,11 @@ public class ProductController {
 		List<Product> listp;
 		
 		listp=productService.getAllProducts();
-	    if (listp.size()==0) throw new ProductNotFoundException();
+	    if (listp.size()==0) throw new ProductNotFoundException("Products are not found");
 	      return new ResponseEntity<>(listp,HttpStatus.OK);
 	   
      }
+	
 	/*
 	 * retrieves specific product using product Id 
 	 * 	product id as parameter 
@@ -41,9 +44,16 @@ public class ProductController {
 	
 	 @RequestMapping("/products/{id}")
 	 
-    public ResponseEntity<Object> getProduct(@PathVariable int id) {
-		Product p= productService.getProduct(id);
-		if(p==null) throw new ProductNotFoundException();
+    public ResponseEntity<Object> getProduct(@PathVariable int id) throws ProductNotFoundException{
+		 Product p;
+		 try {
+		 p= productService.getProduct(id);
+		 }
+		 catch(Exception ex) {
+			 throw new ProductNotFoundException("Product Not Found ");
+		 }
+		
+		
 		 return new ResponseEntity<>(p,HttpStatus.OK);
 	}
 	 /*
@@ -52,10 +62,16 @@ public class ProductController {
 	  */
 	 
 	@RequestMapping(method=RequestMethod.POST,value="/products")
-    public void addProduct(@RequestBody() Product pro) {
-		if(pro!=null) {
+    public ResponseEntity<Object> addProduct(@RequestBody() Product pro) {
+		try {
 		productService.addProduct(pro);
 		}
+		catch(Exception ex) {
+			throw new ProductNotFoundException("Product is not Added");
+		}
+		return new ResponseEntity<>("Product are Added",HttpStatus.OK);
+		
+		
 	}
 	/*
 	 * 	This methods update the  product using  Product Id 
@@ -63,16 +79,27 @@ public class ProductController {
 	 */
 	  @RequestMapping(method=RequestMethod.PUT,value="/products/{id}")
 	 
-   public void updateProduct(@RequestBody Product pro,@PathVariable int id) {
+   public ResponseEntity<Object> updateProduct(@RequestBody Product pro,@PathVariable int id) {
+		  try {
 			productService.updateProduct(pro,id);
+		  }
+		  
+		  catch(Exception ex) {
+			  throw new ProductNotFoundException("Product cant be Updated");
+		  }
+		  return new ResponseEntity<>("Product updated Successfully ",HttpStatus.OK);
 		}
 	  /*
 	   * This method deletes  product using  product Id 
 	   * It takes product Id as Parameter
 	   */
 	@RequestMapping(method=RequestMethod.DELETE,value="/products/{id}")
-	public void deleteTopic(@PathVariable int id) {
+	public ResponseEntity<Object> deleteTopic(@PathVariable int id) {
+		try {
 			productService.deleteProduct(id);
-		
+		}catch(Exception ex) {
+			throw new ProductNotFoundException("The Id is not Associated with any Product");
+		}
+		return new ResponseEntity<>("Product is deleted",HttpStatus.OK);
 	}
 }
